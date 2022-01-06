1b81e96f2b4cf05432c493b72a90d91033b80e19
canadrian72 <patterson.n.adrian@gmail.com>
2022-01-06 11:55:10 -0500

    Updating docker compose so it works

---
+++ ControlCenterUi/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>Hello</p>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
