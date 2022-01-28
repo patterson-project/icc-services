7d8e1a208c2a560f196b3f0c5f97b599dc993f08
julian-patterson <julianedwardpatterson@gmail.com>
2022-01-28 10:10:53 -0500

    adding-sleep-function

--- LedApi/src/api.py
+++ LedApi/src/api.py
@@ -80,6 +80,11 @@ def sunrise() -> Response:
     publish("leds", json.dumps(led_request.__dict__))
     return Response(status=200)
 
+@app.route("/sleep")
+def sleep() -> Response:
+    led_request = LedRequest("sleep")
+    publish("leds", json.dumps(led_request.__dict__))
+    return Response(status=200)
 
 def start():
     app.run(host='0.0.0.0', threaded=True, port=8000, debug=True)
