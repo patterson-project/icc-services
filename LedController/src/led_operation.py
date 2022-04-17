9fe04a685f038e4d06ba9a3eb7eb197bd3390cc5
julian-patterson <julianedwardpatterson@gmail.com>
2022-04-17 18:43:31 -0400

    Trying event loop for async IO

--- LedController/src/led_operation.py
+++ LedController/src/led_operation.py
@@ -12,8 +12,10 @@ def off(strip) -> None:
 
 
 async def rgb(strip, bulb_1, bulb_2, r, g, b, wait_ms=5) -> None:
-    await set_bulb_color(bulb_1, r, g, b)
-    await set_bulb_color(bulb_2, r, g, b)
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(set_bulb_color(bulb_1, r, g, b))
+    event_loop.run_until_complete(set_bulb_color(bulb_2, r, g, b))
+
     for i in range(strip.numPixels()):
         strip.setPixelColorRGB(i, r, b, g)
         strip.show()
