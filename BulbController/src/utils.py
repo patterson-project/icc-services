96e1d15cc4416ab3676c4136addcaa313cb67589
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-04 20:47:50 -0400

    Toning down the \t

--- BulbController/src/utils.py
+++ BulbController/src/utils.py
@@ -21,5 +21,5 @@ class BulbRequest:
 
 def log(message):
     now = datetime.now(timezone.utc).astimezone()
-    print("[" + str(now.strftime("%Y-%m-%d %H:%M:%S")) + "]\t\t", end="")
+    print("[" + str(now.strftime("%Y-%m-%d %H:%M:%S")) + "]\t", end="")
     print(message)
