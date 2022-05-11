1f5ecdd0a3d91913782c593c60e1a1276fcdbcc8
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-11 17:53:23 -0400

    Re-adding kubernetes logic.

--- update_cluster.sh
+++ update_cluster.sh
@@ -4,3 +4,5 @@ cd BulbController/Bulb1 && sh dockerpush.sh && cd ../../
 cd BulbController/Bulb2 && sh dockerpush.sh && cd ../../
 cd LedController && sh dockerpush.sh && cd ../
 cd IotControlCenterUi && sh dockerpush.sh && cd ../
+
+microk8s kubernetes delete --all pods --namespace=default
