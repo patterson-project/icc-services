327f549d363b9b4817b790899bc61efa9eda5fc4
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-12 15:15:05 -0400

    Fixing update script.

--- update_cluster.sh
+++ update_cluster.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+printf "Building and pushing API service to dockerhub..."
 cd Api && sh dockerpush.sh && cd ../
-cd BulbController/Bulb1 && sh dockerpush.sh && cd ../../
-cd BulbController/Bulb2 && sh dockerpush.sh && cd ../../
-cd LedController && sh dockerpush.sh && cd ../
+printf "\nBuilding and pushing UI service to dockerhub..."
 cd IotControlCenterUi && sh dockerpush.sh && cd ../
+printf "\nBuilding and pushing BULB 1 service to dockerhub..."
+cd BulbController/Bulb1 && sh dockerpush.sh && cd ../../
+printf "\nBuilding and pushing BULB 2 service to dockerhub..."
+cd BulbController/Bulb2 && sh dockerpush.sh && cd ../../
+printf "\nBuilding and pushing LED service to dockerhub..."
+cd LedController && sh dockerpush.sh && cd ../
 
+printf "\n\nDeleting and restarting all pods..."
 microk8s kubernetes delete --all pods --namespace=default
+
+printf "\nDone.\n"
