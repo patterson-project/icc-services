52ed1364fcf8857abdf35635fb3fd7ab55f226f7
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-13 13:49:57 -0400

    Adding more changes

--- IotControlCenterUi/src/config.ts
+++ IotControlCenterUi/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
-  LED_STRIP_ENDPOINT: "http://10.0.0.35:8000/lighting/ledstrip",
-  BULB_1_ENDPOINT: "http://10.0.0.35:8000/lighting/bulb1",
-  BULB_2_ENDPOINT: "http://10.0.0.35:8000/lighting/bulb2",
+  LED_STRIP_ENDPOINT: "http://10.0.0.34/lighting/ledstrip",
+  BULB_1_ENDPOINT: "http://10.0.0.34/lighting/bulb1",
+  BULB_2_ENDPOINT: "http://10.0.0.34/lighting/bulb2",
 };
 
 export default config;
