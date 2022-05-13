8e80fd21d687d4623697b11ab8987d2b4f3457b8
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-13 00:24:47 -0400

    Fixing shell script output formatting

--- update_cluster.sh
+++ update_cluster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-printf "Building and pushing API service to dockerhub..."
+printf "Building and pushing API service to dockerhub...\n"
 cd Api && sh dockerpush.sh && cd ../
-printf "\nBuilding and pushing UI service to dockerhub..."
+printf "\nBuilding and pushing UI service to dockerhub...\n"
 cd IotControlCenterUi && sh dockerpush.sh && cd ../
-printf "\nBuilding and pushing BULB 1 service to dockerhub..."
+printf "\nBuilding and pushing BULB 1 service to dockerhub...\n"
 cd BulbController/Bulb1 && sh dockerpush.sh && cd ../../
-printf "\nBuilding and pushing BULB 2 service to dockerhub..."
+printf "\nBuilding and pushing BULB 2 service to dockerhub...\n"
 cd BulbController/Bulb2 && sh dockerpush.sh && cd ../../
-printf "\nBuilding and pushing LED service to dockerhub..."
+printf "\nBuilding and pushing LED service to dockerhub...\n"
 cd LedController && sh dockerpush.sh && cd ../
 
-printf "\n\nDeleting and restarting all pods..."
+printf "\nDeleting and restarting all pods...\n"
 microk8s kubectl delete --all pods --namespace=default
 
 printf "\nDone.\n"
