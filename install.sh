af5d6f13f80434a0b8e1dd1d015f25644ad72c24
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-15 18:13:09 -0400

    fixing installation script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd Api && chmod a+x dockerpush.sh && cd ../
-cd BulbController/Bulb1 && chmod a+x dockerpush.sh && cd ../../
-cd BulbController/Bulb2 && chmod a+x dockerpush.sh && cd ../../
-cd LedController && chmod a+x dockerpush.sh && cd ../
-cd IotControlCenterUi && chmod a+x dockerpush.sh && cd ../
-chmod a+x update_cluster.sh
--- update_cluster.sh
+++ update_cluster.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 printf "Building and pushing API service to dockerhub...\n"
-cd Api && sh dockerpush.sh && cd ../
+cd Api && bash dockerpush.sh && cd ../
 printf "\nBuilding and pushing UI service to dockerhub...\n"
-cd IotControlCenterUi && sh dockerpush.sh && cd ../
+cd IotControlCenterUi && bash dockerpush.sh && cd ../
 printf "\nBuilding and pushing BULB 1 service to dockerhub...\n"
-cd BulbController/Bulb1 && sh dockerpush.sh && cd ../../
+cd BulbController/Bulb1 && bash dockerpush.sh && cd ../../
 printf "\nBuilding and pushing BULB 2 service to dockerhub...\n"
-cd BulbController/Bulb2 && sh dockerpush.sh && cd ../../
+cd BulbController/Bulb2 && bash dockerpush.sh && cd ../../
 printf "\nBuilding and pushing LED service to dockerhub...\n"
-cd LedController && sh dockerpush.sh && cd ../
+cd LedController && bash dockerpush.sh && cd ../
 
 printf "\nDeleting and restarting all pods...\n"
 microk8s kubectl delete --all pods --namespace=default
