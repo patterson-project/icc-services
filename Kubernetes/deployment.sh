eb7bbd8cde90cab3be3d5a3112801beac95078ec
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-16 01:42:05 -0400

    Infrastructure refactoring

---
+++ Kubernetes/deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+printf "Deleting all SERVICES...\n"
+microk8s kubectl delete --all services --namespace=default
+printf "\n"
+for filename in Services/*.yaml; do
+    printf "Applying $filename\n"
+    microk8s kubectl apply -f $filename
+done
+
+printf "\n\nDeleting all INGRESS routing...\n"
+microk8s kubectl delete --all ingress --namespace=default
+printf "\n"
+for filename in Ingress/*.yaml; do
+    printf "Applying $filename\n"
+    microk8s kubectl apply -f $filename
+done
+
+printf "\n\nDeleting all DEPLOYMENTS...\n"
+microk8s kubectl delete --all deployments --namespace=default
+printf "\n"
+for filename in Deployments/*.yaml; do
+    printf "Applying $filename\n"
+    microk8s kubectl apply -f $filename
+done
+
--- deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-microk8s kubernetes delete --all deployments --namespace=default
-
-for filename in Kubernetes/Deployments/*.yaml; do
-    mirok8s kubernetes apply -f $filename
-done
