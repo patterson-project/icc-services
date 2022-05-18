23e6f2953e375780cccc0588d3e142fcf65f4c14
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-18 16:30:01 -0400

    Changing gears and going for an HTTP server.

---
+++ LedController/src/ledserver.py
@@ -0,0 +1,31 @@
+from flask import Flask, Response, request
+from flask_cors import CORS
+from utils import LightingRequest, log
+from ledstrip import LedStripController
+from gevent.pywsgi import WSGIServer
+
+app: Flask = Flask("__main__")
+CORS(app)
+
+led_strip_controller: LedStripController = LedStripController()
+
+
+@app.route("/health")
+def index() -> Response:
+    return Response("Healthy", status=200)
+
+
+@app.route("/lightingrequest", methods=["POST"])
+def led_strip() -> Response:
+    body = request.get_json()
+    led_request = LightingRequest(**body)
+    log(led_request.__dict__)
+
+    led_strip_controller.request = led_request
+    led_strip_controller.operation_callback_by_name[led_request.operation]()
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    http_server = WSGIServer(("", 8000), app)
+    http_server.serve_forever()
--- LightingApi/src/api.py
+++ LightingApi/src/api.py
@@ -1,18 +1,18 @@
 import asyncio
 from flask import Flask, Response, request
 from flask_cors import CORS
-from utils import LightingMqttClient, LightingRequest, CoapRequest, CoapUri
+from utils import LightingMqttClient, LightingRequest
+from gevent.pywsgi import WSGIServer
 
 app: Flask = Flask("__main__")
 CORS(app)
 
 mqtt_client = LightingMqttClient()
-coap_request = CoapRequest()
 
 
-@app.route("/")
+@app.route("/health")
 def index() -> Response:
-    return Response(status=200)
+    return Response("Healthy", status=200)
 
 
 @app.route("/lighting/ledstrip", methods=["POST"])
@@ -20,7 +20,6 @@ def led_strip() -> Response:
     body = request.get_json()
     try:
         led_request = LightingRequest(**body)
-        asyncio.run(coap_request.post(led_request, CoapUri.LED_STRIP_URI))
     except:
         return Response("Invalid JSON body in request.", 400)
 
@@ -52,5 +51,5 @@ def bulb_2() -> Response:
 
 
 if __name__ == "__main__":
-    asyncio.run(coap_request.init_coap_protocol())
-    app.run(host="0.0.0.0", threaded=True, port=8000)
+    http_server = WSGIServer(("", 8000), app)
+    http_server.serve_forever()
