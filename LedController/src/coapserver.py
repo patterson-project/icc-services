c1e69dc2b3d6f252d1671190e17b8c8a14382f8c
canadrian72 <patterson.n.adrian@gmail.com>
2022-05-18 15:32:01 -0400

    Adding weird line from docs.

--- LedController/src/coapserver.py
+++ LedController/src/coapserver.py
@@ -33,6 +33,9 @@ class HealthResource(resource.Resource):
 
 async def main():
     root = resource.Site()
+    root.add_resource(
+        [".well-known", "core"], resource.WKCResource(root.get_resources_as_linkheader)
+    )
     root.add_resource(["lightingrequest"], LightingRequestResource())
     root.add_resource(["health"], HealthResource())
 
