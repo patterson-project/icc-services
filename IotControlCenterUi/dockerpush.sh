b1d491845d04d6e4d6731b6366f04da5beaeb708
julian-patterson <julianedwardpatterson@gmail.com>
2022-06-18 15:22:47 -0400

    bug fix: changed dockerpush in ui to update

--- IotControlCenterUi/dockerpush.sh
+++ IotControlCenterUi/dockerpush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose build
-docker push canadrian72/iot-control-center:bulb-controller
-microk8s kubectl delete pods -l svc=bulb-controller
+docker push canadrian72/iot-control-center:ui
+microk8s kubectl delete pods -l svc=ui
