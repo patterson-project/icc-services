d9c517cc4d04ed91063a5292d0024e4a8e0bc4db
julian-patterson <julianedwardpatterson@gmail.com>
2022-07-07 13:46:07 -0400

    cleaning up code in scene dialog

--- IotControlCenterUi/src/Components/LightingPageComponents/Dialogs/SceneDialog.tsx
+++ IotControlCenterUi/src/Components/LightingPageComponents/Dialogs/SceneDialog.tsx
@@ -1,7 +1,5 @@
 import { Box, Grid, Typography } from "@mui/material";
 import React, { FC } from "react";
-import AnimationIcon from "@mui/icons-material/Animation";
-import IOperationButton from "../../OperationButton";
 import SceneButtonBox from "../LightingComponents/SceneButtonBox";
 import {
   gridContainerStyle,
@@ -10,62 +8,6 @@ import {
   pageDivStyle,
   subHeadingStyle,
 } from "../../../Styles/DialogStyles";
-import { LightingRequest } from "../../../types";
-import { post } from "../../../utils";
-import config from "../../../config";
-
-const buttonBoxStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100%",
-  backgroundColor: "#2C2C2E",
-  width: "90%",
-  borderRadius: "10px",
-  paddingBottom: "10px",
-};
-
-const onClickOcean = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "ocean",
-  };
-  post(config.OCEAN_SCENE_ENDPOINT, sceneRequest);
-};
-
-const onClickRose = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "rose",
-  };
-  post(config.ROSE_SCENE_ENDPOINT, sceneRequest);
-};
-
-const onClickRainbow = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "rainbow",
-  };
-  post(config.RAINBOW_SCENE_ENDPOINT, sceneRequest);
-};
-
-const onClickCandy = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "candy",
-  };
-  post(config.CANDY_SCENE_ENDPOINT, sceneRequest);
-};
-
-const onClickPeachy = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "peachy",
-  };
-  post(config.PEACHY_SCENE_ENDPOINT, sceneRequest);
-};
-
-const onClickJungle = () => {
-  const sceneRequest: LightingRequest = {
-    operation: "jungle",
-  };
-  post(config.JUNGLE_SCENE_ENDPOINT, sceneRequest);
-};
 
 const SceneDialog: FC = () => {
   return (
