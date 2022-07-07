a4ff3f2ebe4bdb9146571e908f7b971e996fd4d0
julian-patterson <julianedwardpatterson@gmail.com>
2022-07-07 13:44:03 -0400

    created a new component for scene button box

--- IotControlCenterUi/src/Components/LightingPageComponents/Dialogs/SceneDialog.tsx
+++ IotControlCenterUi/src/Components/LightingPageComponents/Dialogs/SceneDialog.tsx
@@ -2,6 +2,7 @@ import { Box, Grid, Typography } from "@mui/material";
 import React, { FC } from "react";
 import AnimationIcon from "@mui/icons-material/Animation";
 import IOperationButton from "../../OperationButton";
+import SceneButtonBox from "../LightingComponents/SceneButtonBox";
 import {
   gridContainerStyle,
   gridItemStyle,
@@ -75,54 +76,9 @@ const SceneDialog: FC = () => {
       <Box style={gridItemStyle}>
         <Typography style={subHeadingStyle}>Select a preset scene</Typography>
       </Box>
-      <Grid container spacing={2} style={gridContainerStyle}>
+      <Grid container spacing={1.5} style={gridContainerStyle}>
         <Grid item xs={12} style={gridItemStyle}>
-          <Box style={buttonBoxStyle}>
-            <Grid container spacing={2} style={gridContainerStyle}>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Ocean"}
-                  color={"#2a5cd1"}
-                  onClick={onClickOcean}
-                ></IOperationButton>
-              </Grid>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Rose"}
-                  color={"#f368f7"}
-                  onClick={onClickRose}
-                ></IOperationButton>
-              </Grid>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Candy"}
-                  color={"#ab5dc7"}
-                  onClick={onClickCandy}
-                ></IOperationButton>
-              </Grid>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Rainbow"}
-                  image={"url(/image.png)"}
-                  onClick={onClickRainbow}
-                ></IOperationButton>
-              </Grid>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Peachy"}
-                  color={"#f09f48"}
-                  onClick={onClickPeachy}
-                ></IOperationButton>
-              </Grid>
-              <Grid item xs={6} style={gridItemStyle}>
-                <IOperationButton
-                  operationName={"Jungle"}
-                  color={"#227a1c"}
-                  onClick={onClickJungle}
-                ></IOperationButton>
-              </Grid>
-            </Grid>
-          </Box>
+          <SceneButtonBox></SceneButtonBox>
         </Grid>
       </Grid>
     </div>
---
+++ IotControlCenterUi/src/Components/LightingPageComponents/LightingComponents/SceneButtonBox.tsx
@@ -0,0 +1,116 @@
+import { Box, Grid, Typography } from "@mui/material";
+import React, { FC } from "react";
+import config from "../../../config";
+import {
+  gridContainerStyle,
+  gridItemStyle,
+} from "../../../Styles/DialogStyles";
+import { LightingRequest } from "../../../types";
+import { post } from "../../../utils";
+import IOperationButton from "../../OperationButton";
+
+const buttonBoxStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+  backgroundColor: "#2C2C2E",
+  width: "90%",
+  borderRadius: "10px",
+  paddingBottom: "10px",
+};
+
+const onClickOcean = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "ocean",
+  };
+  post(config.OCEAN_SCENE_ENDPOINT, sceneRequest);
+};
+
+const onClickRose = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "rose",
+  };
+  post(config.ROSE_SCENE_ENDPOINT, sceneRequest);
+};
+
+const onClickRainbow = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "rainbow",
+  };
+  post(config.RAINBOW_SCENE_ENDPOINT, sceneRequest);
+};
+
+const onClickCandy = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "candy",
+  };
+  post(config.CANDY_SCENE_ENDPOINT, sceneRequest);
+};
+
+const onClickPeachy = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "peachy",
+  };
+  post(config.PEACHY_SCENE_ENDPOINT, sceneRequest);
+};
+
+const onClickJungle = () => {
+  const sceneRequest: LightingRequest = {
+    operation: "jungle",
+  };
+  post(config.JUNGLE_SCENE_ENDPOINT, sceneRequest);
+};
+
+const SceneButtonBox: FC = () => {
+  return (
+    <Box style={buttonBoxStyle}>
+      <Grid container spacing={2} style={gridContainerStyle}>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Ocean"}
+            color={"#2a5cd1"}
+            onClick={onClickOcean}
+          ></IOperationButton>
+        </Grid>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Rose"}
+            color={"#f368f7"}
+            onClick={onClickRose}
+          ></IOperationButton>
+        </Grid>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Candy"}
+            color={"#ab5dc7"}
+            onClick={onClickCandy}
+          ></IOperationButton>
+        </Grid>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Rainbow"}
+            image={"url(/image.png)"}
+            onClick={onClickRainbow}
+          ></IOperationButton>
+        </Grid>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Peachy"}
+            color={"#f09f48"}
+            onClick={onClickPeachy}
+          ></IOperationButton>
+        </Grid>
+        <Grid item xs={6} style={gridItemStyle}>
+          <IOperationButton
+            operationName={"Jungle"}
+            color={"#227a1c"}
+            onClick={onClickJungle}
+          ></IOperationButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default SceneButtonBox;
