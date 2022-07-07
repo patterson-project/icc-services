cd47434662b199107ef87671840f389944c739dc
julian-patterson <julianedwardpatterson@gmail.com>
2022-07-07 14:06:14 -0400

    Updated tabs

--- IotControlCenterUi/src/Pages/LightingPage.tsx
+++ IotControlCenterUi/src/Pages/LightingPage.tsx
@@ -67,7 +67,7 @@ const LightingPage: FC = () => {
           icon={<AllInclusiveIcon style={iconStyle} />}
         />
         <BottomNavigationAction
-          label={getBottomNavigationLabelSpan("Power")}
+          label={getBottomNavigationLabelSpan("Devices")}
           value={2}
           icon={<PowerSettingsNewIcon style={iconStyle} />}
         />
