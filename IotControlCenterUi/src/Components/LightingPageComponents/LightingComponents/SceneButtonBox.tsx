a9f4745880e7dfd17b5f8a37c82c9e855a93a430
julian-patterson <julianedwardpatterson@gmail.com>
2022-07-08 13:52:45 -0400

    cleaning up code

--- IotControlCenterUi/src/Components/LightingPageComponents/LightingComponents/SceneButtonBox.tsx
+++ IotControlCenterUi/src/Components/LightingPageComponents/LightingComponents/SceneButtonBox.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import React, { FC } from "react";
 import config from "../../../config";
 import {
