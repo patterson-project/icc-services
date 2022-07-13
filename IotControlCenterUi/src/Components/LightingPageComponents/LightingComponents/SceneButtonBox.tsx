6d949d4c0a14fd536d1caf13a6fc7d1dca53dde2
julian-patterson <julianedwardpatterson@gmail.com>
2022-07-13 12:52:01 -0400

    fixing endpoints as requested on PR review

--- IotControlCenterUi/src/Components/LightingPageComponents/LightingComponents/SceneButtonBox.tsx
+++ IotControlCenterUi/src/Components/LightingPageComponents/LightingComponents/SceneButtonBox.tsx
@@ -24,42 +24,42 @@ const onClickOcean = () => {
   const sceneRequest: LightingRequest = {
     operation: "ocean",
   };
-  post(config.OCEAN_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/ocean", sceneRequest);
 };
 
 const onClickRose = () => {
   const sceneRequest: LightingRequest = {
     operation: "rose",
   };
-  post(config.ROSE_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/rose", sceneRequest);
 };
 
 const onClickRainbow = () => {
   const sceneRequest: LightingRequest = {
     operation: "rainbow",
   };
-  post(config.RAINBOW_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/rainbow", sceneRequest);
 };
 
 const onClickCandy = () => {
   const sceneRequest: LightingRequest = {
     operation: "candy",
   };
-  post(config.CANDY_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/candy", sceneRequest);
 };
 
 const onClickPeachy = () => {
   const sceneRequest: LightingRequest = {
     operation: "peachy",
   };
-  post(config.PEACHY_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/peachy", sceneRequest);
 };
 
 const onClickJungle = () => {
   const sceneRequest: LightingRequest = {
     operation: "jungle",
   };
-  post(config.JUNGLE_SCENE_ENDPOINT, sceneRequest);
+  post(config.SCENE_ENDPOINT + "/jungle", sceneRequest);
 };
 
 const SceneButtonBox: FC = () => {
