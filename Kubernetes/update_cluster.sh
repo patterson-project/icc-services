f8b941f64b9e891a60922ff0d6ab9e1d3bd825a8
canadrian72 <patterson.n.adrian@gmail.com>
2022-07-18 01:08:49 +0000

    fixing kube

--- Kubernetes/update_cluster.sh
+++ Kubernetes/update_cluster.sh
@@ -6,10 +6,10 @@ printf "\nBuilding and pushing UI to dockerhub...\n"
 cd Ui && bash dockerpush.sh && cd ../
 
 printf "\nBuilding and pushing BULB controller to dockerhub...\n"
-cd Controller.KasaBulb && bash dockerpush.sh && cd ../../
+cd Controller.KasaBulb && bash dockerpush.sh && cd ../
 
 printf "\nBuilding and pushing DEVICE service to dockerhub...\n"
-cd Service.DeviceManager && bash dockerpush.sh && cd ../../
+cd Service.DeviceManager && bash dockerpush.sh && cd ../
 
 # printf "\nBuilding and pushing LED service to dockerhub...\n"
 # cd LedController && bash dockerpush.sh && cd ../
