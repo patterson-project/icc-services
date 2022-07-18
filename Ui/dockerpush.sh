544230676d04210ea0543ae0416cd6b87cd981bc
canadrian72 <patterson.n.adrian@gmail.com>
2022-07-17 20:32:13 -0400

    Fixing docker push scripts

--- Ui/dockerpush.sh
+++ Ui/dockerpush.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-printf "Deleting all SERVICES...\n"
-kubectl delete --all services --namespace=default
-printf "\n"
-for filename in Services/*.yaml; do
-    printf "Applying $filename\n"
-    kubectl apply -f $filename
-done
-
-printf "\n\nDeleting all INGRESS routing...\n"
-kubectl delete --all ingress --namespace=default
-printf "\n"
-for filename in Ingress/*.yaml; do
-    printf "Applying $filename\n"
-    kubectl apply -f $filename
-done
-
-printf "\n\nDeleting all DEPLOYMENTS...\n"
-kubectl delete --all deployments --namespace=default
-printf "\n"
-for filename in Deployments/*.yaml; do
-    printf "Applying $filename\n"
-    kubectl apply -f $filename
-done
-
-printf "\n\nDeleting all PODS...\n"
-kubectl delete --all pods --namespace=default
-
-printf "\n\nDone.\n"
+docker-compose build
+docker push canadrian72/iot-control-center:ui
+kubectl delete pods -l svc=ui
