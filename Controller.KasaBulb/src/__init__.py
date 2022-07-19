ae19fd47ceeb9738546af4f1b0f8c191a31a8649
canadrian72 <patterson.n.adrian@gmail.com>
2022-07-18 20:49:44 -0400

    Fixing bulb controller

--- Controller.KasaBulb/src/__init__.py
+++ Controller.KasaBulb/src/__init__.py
@@ -1,8 +1,7 @@
 import asyncio
-import json
 from threading import Thread
 from bson import ObjectId
-from quart import Quart, Response, jsonify, request
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from pymongo.collection import Collection
@@ -14,7 +13,7 @@ from device import Device
 from config import ServiceUris
 
 # Flask app object with CORS
-app = Quart("__main__")
+app = Flask("__main__")
 app.config["MONGO_URI"] = ServiceUris.MONGO_DB_URI
 
 CORS(app)
@@ -29,7 +28,7 @@ bulbs: dict[ObjectId, BulbController] = {}
 loop = asyncio.new_event_loop()
 
 
-async def get_bulb_devices():
+def get_bulb_devices():
     kasa_bulbs = list(
         Device(**device)
         for device in devices.find({"type": "lighting", "model": "Kasa KL-215"})
@@ -52,16 +51,17 @@ def index() -> Response:
 
 
 @app.route("/add/<string:id>", methods=["POST"])
-async def add_bulb(id: str):
+def add_bulb(id: str):
     new_bulb_id = ObjectId(id)
     new_bulb = Device(**devices.find_one_or_404({"_id": new_bulb_id}))
     bulb_controller = BulbController()
-    await bulb_controller.create_bulb(new_bulb.ip)
+    asyncio.run_coroutine_threadsafe(bulb_controller.create_bulb(new_bulb.ip), loop)
     bulbs[new_bulb_id] = bulb_controller
 
 
 @app.route("/update", methods=["PUT"])
 def update_bulbs():
+    bulbs = {}
     get_bulb_devices()
 
 
@@ -71,8 +71,10 @@ def lighting_request() -> Response:
         bulb_request = LightingRequest(**request.get_json())
         bulb_controller = bulbs[bulb_request.id]
         bulb_controller.set_request(bulb_request)
-        await bulb_controller.update_bulb()
-        await bulb_controller.operation_callback_by_name[bulb_request.operation]()
+        asyncio.run_coroutine_threadsafe(bulb_controller.update_bulb(), loop)
+        asyncio.run_coroutine_threadsafe(
+            bulb_controller.operation_callback_by_name[bulb_request.operation](), loop
+        )
         return "Success", 200
     except (SmartDeviceException, TypeError) as e:
         return str(e), 500
