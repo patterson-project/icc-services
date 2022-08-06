027c284349c6122f933e1d456ef412d445507176
canadrian72 <patterson.n.adrian@gmail.com>
2022-08-05 21:11:38 -0400

    Changing URLs

--- Ui/src/config.ts
+++ Ui/src/config.ts
@@ -1,13 +1,13 @@
-const ip = require('ip');
+import ip from 'ip';
 
 const ip_address = ip.address();
 
 const config = {
-  LED_STRIP_ENDPOINT: `${ip_address}/lighting/ledstrip`,
-  BULB_1_ENDPOINT: `${ip_address}/lighting/lighting/bulb1`,
-  BULB_2_ENDPOINT: `${ip_address}/lighting/bulb2`,
-  SCENE_ENDPOINT: `${ip_address}/lighting/scene`,
-  DEVICE_MANAGER_ENDPOINT: `${ip_address}/devices`
+  LED_STRIP_ENDPOINT: `http://${ip_address}/lighting/ledstrip`,
+  BULB_1_ENDPOINT: `http://${ip_address}/lighting/lighting/bulb1`,
+  BULB_2_ENDPOINT: `http://${ip_address}/lighting/bulb2`,
+  SCENE_ENDPOINT: `http://${ip_address}/lighting/scene`,
+  DEVICE_MANAGER_ENDPOINT: `http://${ip_address}/devices`
 };
 
 export default config;
