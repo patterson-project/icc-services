c27609d7a43e13b0969afdc51ed5b1d6dc4923f5
canadrian72 <patterson.n.adrian@gmail.com>
2022-08-05 21:04:29 -0400

    Changing method of obtaining ip

--- Ui/src/config.ts
+++ Ui/src/config.ts
@@ -1,9 +1,13 @@
+const ip = require('ip');
+
+const ip_address = ip.address();
+
 const config = {
-  LED_STRIP_ENDPOINT: "http://lighting-service-cluster-ip.default.svc.cluster.local/lighting/ledstrip",
-  BULB_1_ENDPOINT: "http://lighting-service-cluster-ip.default.svc.cluster.local/lighting/lighting/bulb1",
-  BULB_2_ENDPOINT: "http://lighting-service-cluster-ip.default.svc.cluster.local/lighting/bulb2",
-  SCENE_ENDPOINT: "http://lighting-service-cluster-ip.default.svc.cluster.local/lighting/scene",
-  DEVICE_MANAGER_ENDPOINT: "http://device-manager-cluster-ip.default.svc.cluster.local/devices"
+  LED_STRIP_ENDPOINT: `${ip_address}/lighting/ledstrip`,
+  BULB_1_ENDPOINT: `${ip_address}/lighting/lighting/bulb1`,
+  BULB_2_ENDPOINT: `${ip_address}/lighting/bulb2`,
+  SCENE_ENDPOINT: `${ip_address}/lighting/scene`,
+  DEVICE_MANAGER_ENDPOINT: `${ip_address}/devices`
 };
 
 export default config;
