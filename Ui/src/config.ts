d4ac34d561db5bc1208def62a2bfeaec4e77644b
canadrian72 <patterson.n.adrian@gmail.com>
2022-08-07 19:43:37 +0000

    Fixing URLS

--- Ui/src/config.ts
+++ Ui/src/config.ts
@@ -1,7 +1,7 @@
 
 const config = {
   LED_STRIP_ENDPOINT: `http://10.0.0.86/lighting/ledstrip`,
-  BULB_0_ENDPOINT: `http://10.0.0.86/lighting/lighting/bulb0`,
+  BULB_1_ENDPOINT: `http://10.0.0.86/lighting/lighting/bulb0`,
   BULB_2_ENDPOINT: `http://10.0.0.86/lighting/bulb2`,
   SCENE_ENDPOINT: `http://10.0.0.86/lighting/scene`,
   DEVICE_MANAGER_ENDPOINT: `http://10.0.0.86/devices`
