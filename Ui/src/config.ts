40f3c170fa55b9b66983f1cfa0ce05bf1dbcb064
canadrian72 <patterson.n.adrian@gmail.com>
2022-08-25 21:20:24 -0400

    Fixing domain grabber

--- Ui/src/config.ts
+++ Ui/src/config.ts
@@ -1,10 +1,11 @@
 /* Uncomment and set domain to local db for testing */
-const domain = window.location.href;
+const domain = 'http://'+window.location.hostname.split('.').join('.');
+;
 // const domain = "http://10.0.0.86/";
 
 const config = {
-  DEVICE_MANAGER_SERVICE_ENDPOINT: `${domain}devices`,
-  LIGHTING_SERVICE_ENDPOINT: `${domain}lighting`
+  DEVICE_MANAGER_SERVICE_ENDPOINT: `${domain}/devices`,
+  LIGHTING_SERVICE_ENDPOINT: `${domain}/lighting`
 };
 
 export default config;
