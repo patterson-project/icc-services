29f665922bf4bd9ea731aafa868710c92d36a5b3
canadrian72 <patterson.n.adrian@gmail.com>
2022-09-02 12:39:50 -0400

    Adding logging

--- raspberypi_installer.sh
+++ raspberypi_installer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Colors
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 # Installing docker and docker compose
+printf "%s\n" "${cyn}1. Installing docker and docker compose..."
+sleep 1
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
@@ -14,9 +25,13 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Logging user into dockerhub
+printf "%s\n" "${cyn}2. Log into docker hub"
+sleep 1
 sudo docker login
 
 # Installing Kubernetes
+printf "%s\n" "${cyn}3. Installing k3s..."
+sleep 1
 curl -sfL https://get.k3s.io | sh -
 # sudo apt install -y linux-modules-extra-raspi
 # sudo sed -i '$s/$/ cgroup_memory=1 cgroup_enable=memory/' /boot/cmdline.txt 
@@ -25,6 +40,8 @@ echo "namespace 8.8.8.8" >> /etc/resolv.conf
 echo "namespace 8.8.4.4" >> /etc/resolv.conf
 
 # Setting up environment variables
+printf "%s\n" "${cyn}4. Environment variable setup"
+sleep 1
 echo "Environment variable setup"
 read -p "Choose a database username: " mongo_username
 read -p "Choose a database password: " mongo_password
@@ -39,15 +56,23 @@ echo "DOCKERHUB_USERNAME=$dockerhub_username" >> /etc/environment
 source /etc/environment
 
 # Deploying the MongoDb locally
+printf "%s\n" "${cyn}5. Deploying local Mongo Database..."
+sleep 1
 cd MongoDb
 sudo docker compose up --build -d
+cd ..
 
 # Deploying cluster infrastructure
-cd Kubernetes
-sudo bash deploy_infrastructure.sh
+printf "%s\n" "${cyn}6. Deploying kubernetes cluster infrastructure..."
+sleep 1
+sudo bash Kubernetes/deploy_infrastructure.sh
 
 # Building and deploying cluster
-sudo bash deploy_cluster.sh
+printf "%s\n" "${cyn}7. Deploying kubernetes pods..."
+sleep 1
+sudo bash Kubernetes/deploy_cluster.sh
 
 # Restarting for changes to take effect
+printf "%s\n" "${grn}Done! Restarting for changes to take effect..."
+sleep 2
 sudo reboot
