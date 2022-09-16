748203c41aebc6cadef96a5bfe3ba38afad2ecbd
canadrian72 <patterson.n.adrian@gmail.com>
2022-09-15 20:36:43 -0400

    Cleaning up

--- Kubernetes/deploy_infrastructure.sh
+++ Kubernetes/deploy_infrastructure.sh
@@ -13,21 +13,18 @@ envsubst < Secrets/secrets.yaml | sudo kubectl apply -f -
 printf "\n%s\n" "${cyn}Deploying all SERVICES...${end}"
 sudo kubectl delete --all services --wait=false --namespace=default
 for filename in Services/*.yaml; do
-    printf "%s\n" "Applying $filename"
     envsubst < $filename | sudo kubectl apply -f -
 done
 
 printf "\n%s\n" "${cyn}Deploying all INGRESS routing...${end}"
 sudo kubectl delete --all ingress --wait=false --namespace=default
 for filename in Ingress/*.yaml; do
-    printf "%s\n" "Applying $filename"
     envsubst < $filename | sudo kubectl apply -f -
 done
 
 printf "\n%s\n" "${cyn}Deploying all DEPLOYMENTS...${end}"
 sudo kubectl delete --all deployments --wait=false --namespace=default
 for filename in Deployments/*.yaml; do
-    printf "Applying $filename\n"
     envsubst < $filename | sudo kubectl apply -f -
 done
 
