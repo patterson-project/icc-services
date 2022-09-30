779116bdb39ee572be757c4865d3a685a88ac693
canadrian72 <patterson.n.adrian@gmail.com>
2022-09-29 21:39:53 -0400

    Fixing circular dependency

--- Controller.KasaLedStrip/src/ledstrip.py
+++ Controller.KasaLedStrip/src/ledstrip.py
@@ -1,10 +1,9 @@
 import asyncio
+import math
 import time
 import kasa
 import colorsys
 from lightingrequest import LightingRequest
-from utils import convert_K_to_RGB
-
 
 class LedStrip:
     async def create_strip(self, ip_address: str) -> None:
@@ -65,7 +64,7 @@ class LedStrip:
 
     async def temperature(self):
         await self.terminate_task()
-        (r, g, b) = convert_K_to_RGB(self.request.temperature)
+        (r, g, b) = self.convert_K_to_RGB(self.request.temperature)
         (r, g, b) = (r / 255, g / 255, b / 255)
         (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
         await self.strip.set_hsv(
@@ -90,3 +89,59 @@ class LedStrip:
 
                 await self.strip.set_hsv(i, 100, 100)
                 time.sleep(0.05)
+    
+    def convert_K_to_RGB(self, colour_temperature) -> tuple[int, int, int]:
+        # range check
+        if colour_temperature < 1000:
+            colour_temperature = 1000
+        elif colour_temperature > 40000:
+            colour_temperature = 40000
+
+        tmp_internal = colour_temperature / 100.0
+
+        # red
+        if tmp_internal <= 66:
+            red = 255
+        else:
+            tmp_red = 329.698727446 * math.pow(tmp_internal - 60, -0.1332047592)
+            if tmp_red < 0:
+                red = 0
+            elif tmp_red > 255:
+                red = 255
+            else:
+                red = tmp_red
+
+        # green
+        if tmp_internal <= 66:
+            tmp_green = 99.4708025861 * math.log(tmp_internal) - 161.1195681661
+            if tmp_green < 0:
+                green = 0
+            elif tmp_green > 255:
+                green = 255
+            else:
+                green = tmp_green
+        else:
+            tmp_green = 288.1221695283 * math.pow(tmp_internal - 60, -0.0755148492)
+            if tmp_green < 0:
+                green = 0
+            elif tmp_green > 255:
+                green = 255
+            else:
+                green = tmp_green
+
+        # blue
+        if tmp_internal >= 66:
+            blue = 255
+        elif tmp_internal <= 19:
+            blue = 0
+        else:
+            tmp_blue = 138.5177312231 * \
+                math.log(tmp_internal - 10) - 305.0447927307
+            if tmp_blue < 0:
+                blue = 0
+            elif tmp_blue > 255:
+                blue = 255
+            else:
+                blue = tmp_blue
+
+        return int(red), int(green), int(blue)
