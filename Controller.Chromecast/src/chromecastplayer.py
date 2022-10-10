cb558d77f3a19217fbea2a4bc716d05b5889542a
canadrian72 <patterson.n.adrian@gmail.com>
2022-10-10 14:13:18 -0400

    Fixing cast stream link

--- Controller.Chromecast/src/chromecastplayer.py
+++ Controller.Chromecast/src/chromecastplayer.py
@@ -11,6 +11,6 @@ class ChromecastPlayer:
     def cast_media(self, path: str):
         print(f"Casting http://{os.getenv('MEDIA_DRIVE_IP')}/public/{path}")
         self.chromecast.media_controller.play_media(
-            f"http://{os.getenv('MEDIA_DRIVE_IP')}/public/{path}", content_type="video/mp4")
+            f"http://{os.getenv('MEDIA_DRIVE_IP')}/{path}", content_type="video/mp4")
         self.chromecast.media_controller.block_until_active()
         self.chromecast.media_controller.play()
