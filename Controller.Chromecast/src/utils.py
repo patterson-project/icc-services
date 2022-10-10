367f25472607e3762a69515b5af7bcc8b13a3a72
canadrian72 <patterson.n.adrian@gmail.com>
2022-10-10 13:32:19 -0400

    Reverting

--- Controller.Chromecast/src/utils.py
+++ Controller.Chromecast/src/utils.py
@@ -8,17 +8,20 @@ from repository import DeviceRepository
 def initialize_chromecasts(device_repository: DeviceRepository) -> dict[PydanticObjectId, ChromecastPlayer]:
     chromecast_devices = device_repository.find_all_chromecasts()
     chromecasts: dict[PydanticObjectId, ChromecastPlayer] = {}
+    network_chromecasts: list[pychromecast.Chromecast] = pychromecast.get_chromecasts()[
+        0]
+
+    print(network_chromecasts)
 
     for chromecast_device in chromecast_devices:
-        chromecast_player = pychromecast.get_chromecast_from_host(
-            chromecast_device.ip)
-        chromecasts[chromecast_device.id] = ChromecastPlayer(
-            chromecast_player)
-        print(f"{chromecast_player.cast_info.host} initialized")
+        chromecast_player = next(
+            (cc for cc in network_chromecasts if cc.cast_info.host == chromecast_device.ip), None)
+
+        print(
+            f"Device host: {chromecast_device.ip}, Player: {chromecast_player}")
+        if chromecast_player is not None:
+            chromecasts[chromecast_device.id] = ChromecastPlayer(
+                chromecast_player)
+            print(f"{chromecast_player.cast_info.host} initialized")
 
     return chromecasts
-
-
-app = Flask("__main__")
-dp = DeviceRepository(app)
-initialize_chromecasts(dp)
