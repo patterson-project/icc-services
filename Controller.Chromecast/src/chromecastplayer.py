cc57ee25b98c6cd505cfc0db93e46b2eba67abc5
canadrian72 <patterson.n.adrian@gmail.com>
2022-10-10 13:49:01 -0400

    Waiting to connect before

--- Controller.Chromecast/src/chromecastplayer.py
+++ Controller.Chromecast/src/chromecastplayer.py
@@ -5,6 +5,8 @@ import pychromecast
 class ChromecastPlayer:
     def __init__(self, chromecast: pychromecast.Chromecast):
         self.chromecast = chromecast
+        self.chromecast.wait()
+        print(f"{self.chromecast.cast_info.host} initialized")
 
     def cast_media(self, path: str):
         self.chromecast.media_controller.play_media(
--- Controller.Chromecast/src/utils.py
+++ Controller.Chromecast/src/utils.py
@@ -11,17 +11,12 @@ def initialize_chromecasts(device_repository: DeviceRepository) -> dict[Pydantic
     network_chromecasts: list[pychromecast.Chromecast] = pychromecast.get_chromecasts()[
         0]
 
-    print(network_chromecasts)
-
     for chromecast_device in chromecast_devices:
         chromecast_player = next(
             (cc for cc in network_chromecasts if cc.cast_info.host == chromecast_device.ip), None)
 
-        print(
-            f"Device host: {chromecast_device.ip}, Player: {chromecast_player}")
         if chromecast_player is not None:
             chromecasts[chromecast_device.id] = ChromecastPlayer(
                 chromecast_player)
-            print(f"{chromecast_player.cast_info.host} initialized")
 
     return chromecasts
