4f32173fbd8de26db9dbb68b9b7b2f08273619b2
canadrian72 <patterson.n.adrian@gmail.com>
2022-12-09 17:55:50 -0500

    Power service with fastapi and trying buster slim

---
+++ Service.Power/src/routes/power.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from server.database import AnalyticsRepository, DeviceRepository
+from icc.models import PowerRequestDto, DeviceControllerProxy, PowerRequestRecord
+from datetime import datetime
+from httpx import AsyncClient
+
+router = APIRouter()
+analytics_repository = AnalyticsRepository()
+device_repository = DeviceRepository()
+http_client = AsyncClient()
+
+
+@router.post("", summary="Create a lighting request", response_description="The created device")
+async def create_device(power_request: PowerRequestDto):
+    device = await device_repository.find_by_id(power_request.target_id)
+
+    await http_client.post(DeviceControllerProxy.device_model_to_url.get(
+        device.model) + "/request", json=power_request.to_json())
+
+    request_record = PowerRequestRecord(
+        time=datetime.utcnow().isoformat(), **power_request.__dict__)
+    await analytics_repository.insert(request_record)
+
+    return power_request.to_json()
